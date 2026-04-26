59afd6c0ce676f161185d47c337572e21e0c105d
MoCI-L <liu2336@outlook.com>
2026-04-26 22:13:33 +0800

    删减

--- build/radar_comm/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/moci/radar_serial/install/radar_interfaces/share/radar_interfaces/package.sh"
--- build/radar_interfaces/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/moci/radar_serial/src/radar_interfaces
-# Build directory: /home/moci/radar_serial/build/radar_interfaces
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("radar_interfaces__py")
-subdirs("radar_interfaces__rs")
